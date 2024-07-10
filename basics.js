e7aebfa3a91043f5c037e939688e254042d6c2f8
bigyikess <158577848+bigyikess@users.noreply.github.com>
2024-07-10 09:24:29 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -20,3 +20,47 @@ let g = Math.cos(Math.PI);
 let h = Math.E ** 12
 
 console.log(e, f, g, h)
+
+let firstName = "Andrew";
+let lastName = "Tay";
+let fullName = firstName + " " + lastName;
+let nameLength = fullName.length;
+console.log(fullName + ", Length = " + nameLength);
+
+let language = "Javascript";
+console.log(language[0]);
+console.log(language.substring(2, 5));
+console.log(language.toUpperCase());
+
+// if ... else ...
+let age = 20;
+
+if (age <= 12) {
+    console.log("Hello kid!");
+}
+else if (age <= 18) {
+    console.log("Hello teen!");
+}
+else {
+    console.log("Hello adult!");
+}
+
+// For loop
+let fruits = ["orange", "apple", "pear", "watermelon"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// while loop
+let counter = 10;
+while (counter > 0) {
+    console.log(counter);
+    counter--;
+}
+
+console.log("Happy New Year!")
