180220c9a88239653ec32fc994c4463c9ef7ca10
bigyikess <158577848+bigyikess@users.noreply.github.com>
2024-07-10 09:10:44 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,22 @@
+let myName = "Python";
+myName = "JS";
+const pup = 'woof' // constant, cannot be changed;
+const pn = 'wahooooooooooo';
+let lis = [1, 2, 3, 4, 5, 6];
+let isHuman = true;
 
+
+console.log(myName);
+
+let a = 5 + 6 * 9;
+let b = 89 % 67;
+let c = 90 / 2;
+let d = 2 ** 90;
+console.log(a, b, c, d);
+
+let e = Math.random();
+let f = Math.round(e);
+let g = Math.cos(Math.PI);
+let h = Math.E ** 12
+
+console.log(e, f, g, h)
